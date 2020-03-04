9c657ea5783853ba11bfe449edceb55392ece16e
Aleksandr Alpatkin <web_master.07@mail.ru>
2020-03-04 16:03:02 +0300

    refactor

--- Java/src/main/java/ru/geekbrains/java_one/lesson1/FirstClass.java
+++ Java/src/main/java/ru/geekbrains/java_one/lesson1/FirstClass.java
@@ -4,17 +4,17 @@ public class FirstClass {
 
     //Задание 1
     private static double resultExpression(int a, int b, int c, int d) {
-        return a * (b + (c / d));
+        return a * (b + (c / (d * 1.0)));
     }
 
     //Задание 2
     private static boolean isWithinNumbers(int a, int b) {
-        return ((a + b) >= 10 && (a + b) <= 20) ? true : false;
+        return ((a + b) >= 10 && (a + b) <= 20);
     }
 
     //Задание 3
-    private static void positiveOrNegativeNum(int num) {
-        String result = (num < 0) ? "Отрицательное" : "Положительное";
+    private static void isPositiveOrNegativeNum(int num) {
+        boolean result = (num >= 0);
         System.out.println(result);
     }
 
@@ -24,18 +24,13 @@ public class FirstClass {
     }
 
     //Задание *
-    private static String leapNonleapYears(int year) {
-        if (((year % 4) == 0 && (year % 100) != 0) || ((year) % 4 == 0 && ((year % 100) == 0) && ((year % 400) == 0))) {
-            return "Год високосный!";
-        }
-        else {
-            return "Год не високосный!";
-        }
+    private static boolean isLeapNonleapYears(int year) {
+        return  (((year % 4) == 0 && (year % 100) != 0) || ((year) % 4 == 0 && ((year % 100) == 0) && ((year % 400) == 0)));
     }
 
     public static void main(String[] args) {
         System.out.println(printHello("Александр"));
-        System.out.println(leapNonleapYears(2020));
+        System.out.println(isLeapNonleapYears(2020));
 
     }
 }
