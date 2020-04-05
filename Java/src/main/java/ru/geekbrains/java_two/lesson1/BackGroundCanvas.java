8a20a51f07d2899d6ebd036d6825a5a0f961f543
Aleksandr Alpatkin <web_master.07@mail.ru>
2020-04-05 14:18:35 +0300

    refact BackGroundCanvas class

--- Java/src/main/java/ru/geekbrains/java_two/lesson1/BackGroundCanvas.java
+++ Java/src/main/java/ru/geekbrains/java_two/lesson1/BackGroundCanvas.java
@@ -5,22 +5,23 @@ import java.awt.*;
 public class BackGroundCanvas {
 
     private Color color = null;
-    private float timeChangeBGColor = 0.0f;
+    private float timeSec = 0.0f;
+    private float timeSecChangeBG = 5.0f;
 
     void onCanvasRepainted(GameCanvas canvas, float deltaTime) {
         render(canvas, deltaTime);
     }
 
     void render(GameCanvas canvas, float deltaTime) {
-        timeChangeBGColor += deltaTime;
-        if (timeChangeBGColor > 5.0f) {
+        timeSec += deltaTime;
+        if (timeSec > timeSecChangeBG) {
             color = new Color(
                     (int) (Math.random() * 255),
                     (int) (Math.random() * 255),
                     (int) (Math.random() * 255)
             );
             canvas.setBackground(color);
-            timeChangeBGColor = 0.0f;
+            timeSec = 0.0f;
         }
     }
 
