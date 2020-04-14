296d3962fba07168b4f5b75901ef2371f9cb2ac1
son666 <web_master.07@mail.ru>
2020-04-15 00:45:18 +0300

    add Java_two lesson04

---
+++ Java/src/main/java/ru/geekbrains/java_two/lesson4/ChatServer.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.java_two.lesson4;
+
+public class ChatServer {
+
+    public void start(int port) {
+        System.out.println("Server started at port: " + port);
+    }
+
+    public void stop() {
+        System.out.println("Server stopped");
+    }
+}
---
+++ Java/src/main/java/ru/geekbrains/java_two/lesson4/ServerGUI.java
@@ -0,0 +1,63 @@
+package ru.geekbrains.java_two.lesson4;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ServerGUI extends JFrame implements ActionListener, Thread.UncaughtExceptionHandler {
+    private static final int POS_X = 1000;
+    private static final int POS_Y = 550;
+    private static final int WIDTH = 200;
+    private static final int HEIGHT = 100;
+
+    private final ChatServer server;
+    private final JButton btnStart = new JButton("Start");
+    private final JButton btnStop = new JButton("Stop");
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ServerGUI();
+            }
+        });
+    }
+
+    ServerGUI() {
+        Thread.setDefaultUncaughtExceptionHandler(this);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(POS_X, POS_Y, WIDTH, HEIGHT);
+        setResizable(false);
+        setTitle("Chat server");
+        setAlwaysOnTop(true);
+        setLayout(new GridLayout(1, 2));
+        btnStart.addActionListener(this);
+        btnStop.addActionListener(this);
+
+        add(btnStart);
+        add(btnStop);
+        server = new ChatServer();
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+        if(source == btnStart) {
+            server.start(8080);
+        } else if (source == btnStop) {
+            server.stop();
+        } else {
+            throw new RuntimeException("Unknown source:" + source);
+        }
+    }
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        e.printStackTrace();
+        String msg = String.format("Exception in thread \"%s\" %s: %s\n\tat %s", t.getName(), e.getClass().getCanonicalName(), e.getMessage(), e.getStackTrace()[0]);
+        JOptionPane.showMessageDialog(this, msg, "Exception", JOptionPane.ERROR_MESSAGE);
+        System.exit(1);
+    }
+}
