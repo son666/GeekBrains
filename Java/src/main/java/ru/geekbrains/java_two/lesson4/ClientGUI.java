2772eac22fac37c1b8c1008a9ed45c49310a1d2b
son666 <33750355+son666@users.noreply.github.com>
2020-04-17 12:33:57 +0300

    add Home Work Java_two lesson04 (#11)
    * add Home Work Java_two lesson04
    
    * refact Home Work Java_two lesson04
    
    * refact Home Work Java_two lesson04

--- Java/src/main/java/ru/geekbrains/java_two/lesson4/ClientGUI.java
+++ Java/src/main/java/ru/geekbrains/java_two/lesson4/ClientGUI.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class ClientGUI extends JFrame implements ActionListener, Thread.UncaughtExceptionHandler {
     private static final int WIDTH = 400;
@@ -50,6 +52,8 @@ public class ClientGUI extends JFrame implements ActionListener, Thread.Uncaught
         log.setWrapStyleWord(true);
         log.setEditable(false);
         cbAlwaysOnTop.addActionListener(this);
+        btnSend.addActionListener(this);
+        tfMessage.addActionListener(this);
 
         panelTop.add(tfIPAddress);
         panelTop.add(tfPort);
@@ -73,6 +77,8 @@ public class ClientGUI extends JFrame implements ActionListener, Thread.Uncaught
         Object obj = e.getSource();
         if (obj == cbAlwaysOnTop) {
             setAlwaysOnTop(cbAlwaysOnTop.isSelected());
+        } else if (obj == btnSend || obj == tfMessage) {
+            sendToLogMessage();
         } else {
             throw new RuntimeException("Unknown source:" + obj);
         }
@@ -81,8 +87,45 @@ public class ClientGUI extends JFrame implements ActionListener, Thread.Uncaught
     @Override
     public void uncaughtException(Thread t, Throwable e) {
         e.printStackTrace();
-        String msg = String.format("Exception in thread \"%s\" %s: %s\n\tat %s", t.getName(), e.getClass().getCanonicalName(), e.getMessage(), e.getStackTrace()[0]);
+        String msg;
+        StackTraceElement[] ste = e.getStackTrace();
+        if (ste.length == 0) {
+            msg = "Empty Stacktrace";
+        } else {
+            msg = String.format("Exception in thread \"%s\" %s: %s\n\tat %s",
+                    t.getName(), e.getClass().getCanonicalName(), e.getMessage(), ste[0]);
+        }
         JOptionPane.showMessageDialog(this, msg, "Exception", JOptionPane.ERROR_MESSAGE);
         System.exit(1);
+    }
+
+    private void sendToLogMessage() {
+        String msg = tfMessage.getText();
+        String user = tfLogin.getText();
+        if ("".equals(msg)) return;
+        tfMessage.setText(null);
+        tfMessage.requestFocusInWindow();
+        putLog(String.format("%s: %s", user, msg));
+        saveToFile(msg, user);
+    }
+
+    private void putLog(String message) {
+        if ("".equals(message)) return;
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                log.append(message + System.lineSeparator());
+                log.setCaretPosition(log.getDocument().getLength());
+            }
+        });
+    }
+
+    private void saveToFile(String msg, String user) {
+        try (FileWriter fileWriter = new FileWriter("E:\\logChat.txt", true)) {
+            fileWriter.write(user + " : " + msg + System.lineSeparator());
+            fileWriter.flush();
+        } catch (IOException e) {
+            uncaughtException(Thread.currentThread(), e);
+        }
     }
 }
