8df7edcb679906c230c9ece8a47eac150bf7947a
son666 <33750355+son666@users.noreply.github.com>
2020-05-27 23:49:46 +0300

    prepare Home Work code lesson05 (#19)

---
+++ Java/src/main/java/ru/geekbrains/java_three/lesson05/Stage.java
@@ -0,0 +1,10 @@
+package ru.geekbrains.java_three.lesson05;
+
+public abstract class Stage {
+    protected int length;
+    protected String description;
+    public String getDescription() {
+        return description;
+    }
+    public abstract void go(Car c);
+}
