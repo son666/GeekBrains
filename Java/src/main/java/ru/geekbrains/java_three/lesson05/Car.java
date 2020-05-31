5277f16ed764dd9367ccd60f0dfd531ff0fcac59
son666 <33750355+son666@users.noreply.github.com>
2020-05-31 19:03:13 +0300

    add Home Work Java_three lesson05 (#20)

--- Java/src/main/java/ru/geekbrains/java_three/lesson05/Car.java
+++ Java/src/main/java/ru/geekbrains/java_three/lesson05/Car.java
@@ -1,36 +1,57 @@
 package ru.geekbrains.java_three.lesson05;
 
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
+    private static AtomicInteger ai;
+
     static {
         CARS_COUNT = 0;
+        ai = new AtomicInteger(0);
     }
+
     private Race race;
     private int speed;
     private String name;
+    private CyclicBarrier barrier;
+
     public String getName() {
         return name;
     }
+
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+
+    public Car(Race race, int speed, CyclicBarrier barrier) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.barrier = barrier;
     }
+
     @Override
     public void run() {
         try {
             System.out.println(this.name + " готовится");
-            Thread.sleep(500 + (int)(Math.random() * 800));
+            Thread.sleep(500 + (int) (Math.random() * 800));
             System.out.println(this.name + " готов");
+            barrier.await();
+            barrier.await();
+            for (int i = 0; i < race.getStages().size(); i++) {
+                race.getStages().get(i).go(this);
+            }
+
+            if (ai.incrementAndGet() == 1) {
+                System.out.println(name + " - WIN!!!");
+            }
+            barrier.await();
+
         } catch (Exception e) {
             e.printStackTrace();
-        }
-        for (int i = 0; i < race.getStages().size(); i++) {
-            race.getStages().get(i).go(this);
         }
     }
 }
--- Java/src/main/java/ru/geekbrains/java_three/lesson05/Tunnel.java
+++ Java/src/main/java/ru/geekbrains/java_three/lesson05/Tunnel.java
@@ -1,25 +1,31 @@
 package ru.geekbrains.java_three.lesson05;
 
+import java.util.concurrent.Semaphore;
+
 public class Tunnel extends Stage {
 
+    private Semaphore smp;
+
     public Tunnel() {
+        this.smp = new Semaphore(MainClass.CARS_COUNT / 2);
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
     }
+
     @Override
     public void go(Car c) {
         try {
-            try {
+            if (!smp.tryAcquire()) {
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
-                System.out.println(c.getName() + " начал этап: " + description);
-                Thread.sleep(length / c.getSpeed() * 1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } finally {
-                System.out.println(c.getName() + " закончил этап: " + description);
+                smp.acquire();
             }
-        } catch (Exception e) {
+            System.out.println(c.getName() + " начал этап: " + description);
+            Thread.sleep(length / c.getSpeed() * 1000);
+        } catch (InterruptedException e) {
             e.printStackTrace();
+        } finally {
+            System.out.println(c.getName() + " закончил этап: " + description);
+            smp.release();
         }
     }
 }
